a5ba0833015c5018a43aafca1f26ba05a3a631a1
Alysson Barrera <alyssonbarrera@outlook.com>
2024-09-14 10:32:41 -0300

    📦 add casl

---
+++ packages/auth/src/index.ts
@@ -0,0 +1,27 @@
+import {
+  AbilityBuilder,
+  type CreateAbility,
+  createMongoAbility,
+  type ForcedSubject,
+  type MongoAbility,
+} from '@casl/ability'
+
+const actions = ['manage', 'invite', 'delete'] as const
+const subjects = ['User', 'all'] as const
+type AppAbilities = [
+  (typeof actions)[number],
+  (
+    | (typeof subjects)[number]
+    | ForcedSubject<Exclude<(typeof subjects)[number], 'all'>>
+  ),
+]
+
+export type AppAbility = MongoAbility<AppAbilities>
+export const createAppAbility = createMongoAbility as CreateAbility<AppAbility>
+
+const { build, can, cannot } = new AbilityBuilder(createAppAbility)
+
+can('invite', 'User')
+cannot('delete', 'User')
+
+export const ability = build()
