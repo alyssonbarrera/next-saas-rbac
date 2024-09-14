92009ad069decca9e94e69d4b25beea7708d8cb1
Alysson Barrera <alyssonbarrera@outlook.com>
2024-09-14 10:44:40 -0300

    🛂 add permissions

---
+++ packages/auth/src/permissions.ts
@@ -0,0 +1,21 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import type { AbilityBuilder } from '@casl/ability'
+
+import type { AppAbility } from './index'
+import { User } from './models/user'
+
+export type Role = 'ADMIN' | 'MEMBER'
+
+type PermissionsByRole = (
+  user: User,
+  builder: AbilityBuilder<AppAbility>,
+) => void
+
+export const permissions: Record<Role, PermissionsByRole> = {
+  ADMIN: (_user, { can }) => {
+    can('manage', 'all')
+  },
+  MEMBER: (_user, { can }) => {
+    can('invite', 'User')
+  },
+}
