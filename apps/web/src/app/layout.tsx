26559a1149d5fca9d18fc91fddc4fb836411368d
Alysson Barrera <alyssonbarrera@outlook.com>
2024-09-19 14:14:51 -0300

    ✨ setup shadcn

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,10 +1,16 @@
 import './globals.css'
 
 import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
 
 export const metadata: Metadata = {
   title: 'Next SaaS RBAC',
 }
+
+const inter = Inter({
+  variable: '--font-inter',
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+})
 
 export default function RootLayout({
   children,
@@ -12,8 +18,8 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" className="dark">
+      <body className={`${inter.className}`}>{children}</body>
     </html>
   )
 }
