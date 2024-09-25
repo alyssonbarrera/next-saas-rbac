613c8507d50ac02a5d38ae7463c200356a0a46be
Alysson Barrera <alyssonbarrera@outlook.com>
2024-09-25 10:29:03 -0300

    🐛 fixes to build

--- apps/web/src/app/auth/sign-in/page.tsx
+++ apps/web/src/app/auth/sign-in/page.tsx
@@ -1,5 +1,11 @@
+import { Suspense } from 'react'
+
 import { SignInForm } from '@/components/forms/sign-in-form'
 
 export default function SignInPage() {
-  return <SignInForm />
+  return (
+    <Suspense>
+      <SignInForm />
+    </Suspense>
+  )
 }
--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -10,6 +10,7 @@ export const metadata: Metadata = {
 }
 
 const inter = Inter({
+  subsets: ['latin'],
   variable: '--font-inter',
   weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
 })
